86a7eab9dc34c5f99c8807ff70d81142932c57c0
pooja-sakhiya-92 <64467965+pooja-sakhiya-92@users.noreply.github.com>
2021-09-09 18:07:29 +0530

    Add mini statement

--- src/main/java/com/home/accountsservice/service/AccountService.java
+++ src/main/java/com/home/accountsservice/service/AccountService.java
@@ -62,9 +62,12 @@ import java.util.Optional;
 	}
 
 	public void showhistory(Long accNum) {
-		System.out.println(accNum);
+		System.out.println(accNum + "--history");
 	}
 
+	public  void showMiniStatement(Long accNum) {
+		System.out.println(accNum + "--ministatement");
+	}
 	//TODO
 	/*Add methods for
 	 * Credit
