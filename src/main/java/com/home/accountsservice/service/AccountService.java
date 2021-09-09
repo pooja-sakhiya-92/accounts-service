9bb9fc7dd0f1e86da6c676135612dc3b547596aa
pooja-sakhiya-92 <64467965+pooja-sakhiya-92@users.noreply.github.com>
2021-09-09 17:27:49 +0530

    add balance 65000

--- src/main/java/com/home/accountsservice/service/AccountService.java
+++ src/main/java/com/home/accountsservice/service/AccountService.java
@@ -10,56 +10,55 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-@Service
-public class AccountService {
+@Service public class AccountService {
 
-    @Autowired
-    private AccountRepository accountRepository;
+	@Autowired private AccountRepository accountRepository;
 
-    public Account getByAccountNumber (String accountNumber) throws AccountException {
-        return Optional.ofNullable ( accountRepository.findById (BigDecimal.valueOf (Long.valueOf ( accountNumber) ))).
-                get().orElse (  null);
-    }
+	public Account getByAccountNumber(String accountNumber) throws AccountException {
+		return Optional.ofNullable(accountRepository.findById(BigDecimal.valueOf(Long.valueOf(accountNumber)))).get()
+				.orElse(null);
+	}
 
-    public Account getAccountNumber (BigDecimal accountNumber) throws AccountException {
-       // Optional<Optional<Account>> account = Optional.ofNullable ( accountRepository.findById ( BigDecimal.valueOf ( Long.valueOf ( accountNumber ) ) ) );
+	public Account getAccountNumber(BigDecimal accountNumber) throws AccountException {
+		// Optional<Optional<Account>> account = Optional.ofNullable ( accountRepository.findById ( BigDecimal.valueOf ( Long.valueOf ( accountNumber ) ) ) );
 
-        Optional<Account> account = accountRepository.findById ( accountNumber );
-        if(account.isPresent ())
-            return  account.get ();
-        else
-            throw new AccountException ( "Account does not exist" );
-    }
+		Optional<Account> account = accountRepository.findById(accountNumber);
+		if (account.isPresent())
+			return account.get();
+		else
+			throw new AccountException("Account does not exist");
+	}
 
-    public Account saveOrUpdate (Account account) throws AccountException {
+	public Account saveOrUpdate(Account account) throws AccountException {
 
-        //Pre conditions check
-        checkElligibility ( account );
-        return accountRepository.save ( account );
-    }
+		//Pre conditions check
+		checkElligibility(account);
+		return accountRepository.save(account);
+	}
 
-    private boolean checkElligibility (Account account) throws AccountException {
-        if (2000 > account.getBalance ( ).intValue ( ))
-            throw new AccountException ( "Please provide balance greater than 2000" );
-        return true;
-    }
+	private boolean checkElligibility(Account account) throws AccountException {
+		if (2000 > account.getBalance().intValue())
+			throw new AccountException("Please provide balance greater than 2000");
+		return true;
+	}
 
-    public Account addBalance(Long accountNumber,Long creditAmount) throws AccountException {
-        Account toCreditAccount = getByAccountNumber ( String.valueOf ( accountNumber ) );
-        toCreditAccount.setBalance ( BigDecimal.valueOf ( toCreditAccount.getBalance ().longValue () + creditAmount ));
-        return accountRepository.save ( toCreditAccount );
-    }
+	public Account addBalance(Long accountNumber, Long creditAmount) throws AccountException {
+		Account toCreditAccount = getByAccountNumber(String.valueOf(accountNumber));
+		toCreditAccount.setBalance(BigDecimal.valueOf(toCreditAccount.getBalance().longValue() + creditAmount));
+		return accountRepository.save(toCreditAccount);
+	}
 
-    public void creditBalance(Long amount) {
+	public void creditBalance(Long amount) {
+		Long newAmount = amount + 65000;
         System.out.println(amount);
-    }
+	}
 
-    //TODO
-    /*Add methods for
-     * Credit
-     * debit
-     * view transactions
-     * show account details
-     *
-     * */
+	//TODO
+	/*Add methods for
+	 * Credit
+	 * debit
+	 * view transactions
+	 * show account details
+	 *
+	 * */
 }
